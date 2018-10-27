1b689da0c7ed3b13e04ecc924f9570c5eec7f8fa
amisolution <siriusgem1@gmail.com>
2018-10-27 16:49:33 +0200

    Update welcome.tsx

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -7,10 +7,10 @@ export class Welcome extends React.Component<Props, {}> {
     public render(): React.ReactNode {
         return (
             <Content>
-                The 0x sandbox provides a runnable example of how to use and interact with 0x. Be sure to check out the{' '}
-                <a href="https://github.com/0xProject/0x-starter-project/">0x starter project</a> for more examples and
-                scenarios. Our <a href="https://0xproject.com/portal">Portal</a> provides a user on-boarding flow to get
-                you started with the 0x ecosystem. Documentation for 0x.js can be found on our{' '}
+                The AMIS 0x v2 sandbox boilerplate provides a runnable example of how to use and interact with 0x v2. Be
+                sure to check out the <a href="https://github.com/0xProject/0x-starter-project/">0x starter project</a>{' '}
+                for more examples and scenarios. Our <a href="https://0xproject.com/portal">Portal</a> provides a user
+                on-boarding flow to get you started with the 0x ecosystem. Documentation for 0x.js can be found on{' '}
                 <a href="https://0xproject.com/docs/0xjs">website</a>.
             </Content>
         );
